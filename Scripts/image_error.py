9dd59e5495921764e141da76bb0c27323ebd4e22
unknown <ldfrank533@outlook.com>
2021-02-11 13:28:19 +0800

    use mse instead of rmse

--- Scripts/image_error.py
+++ Scripts/image_error.py
@@ -3,8 +3,8 @@ import sys
 import numpy as np
 
 
-def rmse(img_0,img_1):
-    return np.sqrt(np.mean((img_0-img_1)**2))
+def mse(img_0,img_1):
+    return np.mean((img_0-img_1)**2)
 
 if __name__ == "__main__":
     img_file_0 = sys.argv[1]
@@ -13,6 +13,6 @@ if __name__ == "__main__":
     img_0 = cv2.imread(img_file_0)
     img_1 = cv2.imread(img_file_1)
 
-    error = rmse(img_0,img_1)
+    error = mse(img_0,img_1)
     
-    print(f"Root Mean Squared Error: {error}")
+    print(f"Mean Squared Error: {error}")
